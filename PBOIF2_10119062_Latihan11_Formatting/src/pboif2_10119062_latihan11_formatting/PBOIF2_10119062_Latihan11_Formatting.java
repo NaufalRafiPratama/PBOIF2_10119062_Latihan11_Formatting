059bd908f3e36636e8f931a940d3f6b09b006051
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:28:04 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan11_Formatting/src/pboif2_10119062_latihan11_formatting/PBOIF2_10119062_Latihan11_Formatting.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan11_formatting;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan11_Formatting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+        
+    }
+    
+}
